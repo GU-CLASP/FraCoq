1ab7ef66958aa48981d506574b326a8a8c9c951d
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2018-10-05 14:03:33 +0200

    tree bank parser

---
+++ BankParser.hs
@@ -0,0 +1,49 @@
+module BankParser (parseBank, SExpr(..)) where
+-- import Text.ParserCombinators.Parsek
+import Text.ParserCombinators.Parsek.Position
+-- import Text.ParserCombinators.Class
+import Data.Char
+
+data SExpr = Atom String | SExpr [SExpr] | Variants deriving Show
+
+keyword :: String -> Parser ()
+keyword x = spaces >> string x >> return ()
+
+identifier :: Parser String
+identifier = spaces >> munch1 (\x -> isAlphaNum x || x `elem` "_")
+
+parens :: Parser x -> Parser x
+parens a = do
+  keyword "("
+  x <- a
+  keyword ")"
+  return x
+
+expr :: Parser SExpr
+expr = (Atom <$> identifier) <|> (SExpr <$> parens exprs) <|> (keyword "variants{}" >> pure Variants)
+
+exprs :: Parser [SExpr]
+exprs = sepBy1 expr space
+
+def :: Parser (String, SExpr)
+def = do
+  keyword "lin"
+  x <- identifier
+  keyword "="
+  val <- expr
+  keyword ";"
+  return (x,val)
+
+bank :: Parser [(String, SExpr)]
+bank = do
+  _prelude <- munch (\x -> x /= '{')
+  keyword "{"
+  keyword "lincat FraCaSPhrase = SS;"
+  defs <- many def
+  keyword "}"
+  return defs
+
+
+parseBank :: IO (ParseResult SourcePos [(String, SExpr)])
+parseBank = parseFromFile bank longestResult "FraCaS-treebank/src/FraCaSBankI.gf"
+
--- shell.nix
+++ shell.nix
@@ -1,7 +1,20 @@
-{ nixpkgs ? import <nixpkgs> {} }:
-with nixpkgs.pkgs;
-stdenv.mkDerivation {
+# { nixpkgs ? import <nixpkgs> {} }:
+let distro = fetchTarball "https://github.com/NixOS/nixpkgs/archive/4a2340ff6bd0474d9a3e933f28b8568c59019b82.tar.gz";
+             # fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-18.09.tar.gz 
+in with import distro {};
+let myGHC = haskellPackages.ghcWithPackages
+                     (haskellPackages: with haskellPackages; [
+                       # libraries
+                       parsek
+                       # tools
+                       cabal-install
+                       mtl
+                       split
+                     ]);
+
+in stdenv.mkDerivation {
   name = "fracoq-env-0";
-  buildInputs = [ coq haskellPackages.gf ];
+  buildInputs = [ coq haskellPackages.gf myGHC ];
 }
+
 
