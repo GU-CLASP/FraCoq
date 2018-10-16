0c78ed2618925291584c96850a86b0d737c479eb
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2018-10-16 11:48:04 +0200

    note

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 # { nixpkgs ? import <nixpkgs> {} }:
 let distro = fetchTarball "https://github.com/NixOS/nixpkgs/archive/4a2340ff6bd0474d9a3e933f28b8568c59019b82.tar.gz";
-             # fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-18.09.tar.gz 
+             # fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-18.09.tar.gz  # CGI broken here (and GF strangely depends on it)
 in with import distro {};
 let myGHC = haskellPackages.ghcWithPackages
                      (haskellPackages: with haskellPackages; [
