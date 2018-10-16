e0b4861a877943879bf493a42affa0005b7252f9
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2018-10-16 14:47:51 +0200

    redo 122 analysis

--- Tests.hs
+++ Tests.hs
@@ -7,55 +7,56 @@ main :: IO ()
 main = do
   suite
   print "----------"
-  evalDbg p_129
-
-
+  evalDbg p_122
 
 -- >>> evalDbg (s_120_1_p ### s_120_2_p)
 -- ((EXISTS a:(meeting_N a). (attend_V2 smith_PN a)) /\ (chair_V2 smith_PN a))
 
 
--- Analysis f;r 122
--- H1 : (forall a, committee_N a -> (exists b, chairman_N b /\ have_V2 a b))
--- H2 : (exists d, member_N d /\ have_V2 a d /\ by8agent_Prep d (fun x1 => (exists c, appoint_V2 x1 c)) b)
--- ---------------------------------------------------------------------------------------------------------
--- H  : (forall e, committee_N e -> (exists h, chairman_N h /\ (exists f, appoint_V2 f h) /\ (exists g, possess_Prep e member_N g /\ by8agent_Prep g (have_V2 e) h)))
+-- Analysis for 122
+
+
+-- forall a, committee_N a -> (exists b, chairman_N b /\ have_V2 a b)
+-- by8agent_Prep (THE (fun x1 => (have_V2 a x1 /\ member_N x1))) (fun x1 => (exists c, appoint_V2 x1 c)) b
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- forall d, committee_N d -> (exists g, chairman_N g /\ (exists e, appoint_V2 e g) /\ (exists f, possess_Prep d member_N f /\ by8agent_Prep f (have_V2 d) g))
+
+
+-- committee_N d
+-- forall a, committee_N a -> (exists b, chairman_N b /\ have_V2 a b)
+-- by8agent_Prep (THE (fun x1 => (have_V2 a x1 /\ member_N x1))) (fun x1 => (exists c, appoint_V2 x1 c)) b
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- (exists g, chairman_N g /\ (exists e, appoint_V2 e g) /\ (exists f, possess_Prep d member_N f /\ by8agent_Prep f (have_V2 d) g))
 
 
 
--- com
--- H1 : (forall a, committee_N a -> (exists b, chairman_N b /\ have_V2 a b))
--- H2 : (exists d, member_N d /\ have_V2 a d /\ by8agent_Prep d (fun x1 => (exists c, appoint_V2 x1 c)) b)
--- he  : committee_N com
--- ---------------------------------------------------------------------------------------------------------
--- H  : (exists h, chairman_N h /\ (exists f, appoint_V2 f h) /\ (exists g, possess_Prep e member_N g /\ by8agent_Prep g (have_V2 e) h))
+-- d : indiv
+-- committee_N d
+-- exists b, chairman_N b /\ have_V2 d b
+-- by8agent_Prep (THE (fun x1 => (have_V2 a x1 /\ member_N x1))) (fun x1 => (exists c, appoint_V2 x1 c)) b
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- (exists g, chairman_N g /\ (exists e, appoint_V2 e g) /\ (exists f, possess_Prep d member_N f /\ by8agent_Prep f (have_V2 d) g))
 
 
-
--- H1 : (exists b, chairman_N b /\ have_V2 com b)
--- H2 : (exists d, member_N d /\ have_V2 com d /\ by8agent_Prep d (fun x1 => (exists c, appoint_V2 x1 c)) b)
--- ---------------------------------------------------------------------------------------------------------
--- H  : (exists h, chairman_N h /\ (exists f, appoint_V2 f h) /\ (exists g, possess_Prep e member_N g /\ by8agent_Prep g (have_V2 e) h))
-
-
--- _ : committee_N com
--- _ : chairman_N b
--- _ : have_V2 com b
--- _ : member_N d
--- _ : have_V2 com d
--- _ : by8agent_Prep d (fun x1 => (exists c, appoint_V2 x1 c)) b
--- ---------------------------------------------------------------------------------------------------------
--- H  : (exists h, chairman_N h /\ (exists f, appoint_V2 f h) /\ (exists g, possess_Prep e member_N g /\ by8agent_Prep g (have_V2 e) h))
+-- d : indiv
+-- b : indiv
+-- committee_N d
+-- chairman_N b
+-- have_V2 d b
+-- by8agent_Prep (THE (fun x1 => (have_V2 a x1 /\ member_N x1))) (fun x1 => (exists c, appoint_V2 x1 c)) b
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- (exists g, chairman_N g /\ (exists e, appoint_V2 e g) /\ (exists f, possess_Prep d member_N f /\ by8agent_Prep f (have_V2 d) g))
 
 
--- _ : committee_N com
--- _ : chairman_N b
--- _ : have_V2 com b
--- _ : member_N d
--- _ : have_V2 com d
--- _ : by8agent_Prep d (fun x1 => (exists c, appoint_V2 x1 c)) b
--- ---------------------------------------------------------------------------------------------------------
---     (exists f, appoint_V2 f b) /\ (exists g, possess_Prep e member_N g /\ by8agent_Prep g (have_V2 e) b))
+-- d : indiv
+-- b : indiv
+-- committee_N d
+-- chairman_N b
+-- have_V2 d b
+-- by8agent_Prep (THE (fun x1 => (have_V2 a x1 /\ member_N x1))) (fun x1 => (exists c, appoint_V2 x1 c)) b
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- (exists e, appoint_V2 e b) /\ (exists f, possess_Prep d member_N f /\ by8agent_Prep f (have_V2 d) b))
+
 
 
 
