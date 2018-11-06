409cca805f53bba9978cd4b1de2c1b51bdf5c0c2
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2018-11-06 09:43:35 +0100

    wibble

--- Section1.v
+++ Section1.v
@@ -4,9 +4,7 @@ Load FraCoq2.
 Theorem T002a: Problem002aTrue.
 cbv. destruct great_A as [great].
 intro H.
-destruct H as [H1 H2].
-destruct H2 as [x H2].
-destruct H2 as [H2 H3].
+destruct H as [H1 [x [H2 H3]]].
 exists x.
 split.
 split.
