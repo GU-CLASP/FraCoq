56e433c0afcc4c2a4d20631a46475ecebb812968
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2019-10-21 14:02:46 +0200

    some more proofs

--- Section7.v
+++ Section7.v
@@ -18,9 +18,34 @@ rewrite <- P2.
 apply y1993before1996.
 Qed.
 
+Theorem  problem255aTrue : Problem255aTrue.
+cbv.
+intros [P1 P2].
+destruct P1 as [loss [isLoss P1']].
+exists loss.
+split.
+assumption.
+apply P1'.
+apply y1992before1993.
+rewrite <- P2.
+apply y1993before1996.
+Qed.
 
 Transparent PN2object.
 Theorem  problem261atrue : Problem261aTrue.
+cbv.
+intros [P1 P2].
+destruct P1 as [jTime P1'].
+destruct P2 as [aTime P2'].
+exists smithTime. 
+intros andersonTime andersonLeft.
+split.
+apply P2'.
+Qed.
+
+
+Transparent PN2object.
+Theorem  problem262atrue : Problem262aTrue.
 cbv.
 firstorder.
 Qed.
